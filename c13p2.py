e224856f3a81ffc5198f384c10548add467bb5d7
SAGAR T <ssagu6661@gmail.com>
2024-08-18 10:54:46 -0400

    Add files via upload

---
+++ c13p2.py
@@ -0,0 +1,16 @@
+# LIST
+print("NUMBER LIST")
+sagar_list =["1233","2345","3456","4569"]
+print(sagar_list)
+print("=========================")
+print("\n")
+print("After Adding Elements")
+sagar_list.insert(1, "1234")
+print(sagar_list)
+print("\n")
+print("=========================")
+print("After changing 1st list number ")
+sagar_list[0]=2023
+print(sagar_list)
+print("\n")
+print("\n")
